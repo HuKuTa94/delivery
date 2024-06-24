9309e5a2b53b41c70ef4c3477c74e5948cf05467
Nikita Koshelev <hukuta94@users.noreply.github.com>
2024-06-24 14:35:43 +0300

    add value object Money

---
+++ core/src/main/kotlin/github/com/hukuta94/delivery/core/domain/sharedkernel/Currency.kt
@@ -0,0 +1,7 @@
+package github.com.hukuta94.delivery.core.domain.sharedkernel
+
+enum class Currency {
+    RUB,
+    KZT,
+    BONUS,
+}
---
+++ core/src/main/kotlin/github/com/hukuta94/delivery/core/port/Exchange.kt
@@ -0,0 +1,15 @@
+package github.com.hukuta94.delivery.core.port
+
+import github.com.hukuta94.delivery.core.domain.sharedkernel.Currency
+import java.math.BigDecimal
+
+interface Exchange {
+
+    /**
+     * @param origin currency to be converted from
+     * @param target currency to be converted into
+     *
+     * @return conversion rate origin currency to target currency
+     */
+    fun rate(origin: Currency, target: Currency): BigDecimal
+}
