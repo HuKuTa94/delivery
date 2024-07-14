05be5202860ec6e3914f66d2e41c28df64ea105f
Nikita Koshelev <hukuta94@users.noreply.github.com>
2024-07-15 01:13:50 +0300

    move configuration of grpc client into separated class

--- api/src/main/kotlin/github/com/hukuta94/delivery/api/startup/configuration/PortConfiguration.kt
+++ api/src/main/kotlin/github/com/hukuta94/delivery/api/startup/configuration/PortConfiguration.kt
@@ -1,11 +1,34 @@
 package github.com.hukuta94.delivery.api.startup.configuration
 
 import github.com.hukuta94.delivery.infrastructure.adapter.grpc.GeoServiceGrpcClient
+import io.grpc.ManagedChannel
+import io.grpc.ManagedChannelBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import javax.annotation.PreDestroy
 
 @Configuration
 open class PortConfiguration {
+
+    private lateinit var geoServiceGrpChannel: ManagedChannel
+
     @Bean
-    open fun getLocationPort() = GeoServiceGrpcClient()
+    open fun geoServiceGrpChannelBean(): ManagedChannel {
+        return ManagedChannelBuilder.forAddress("localhost", 8084)
+            .usePlaintext() // disable SSL/TLS
+            .build()
+    }
+
+    @PreDestroy
+    fun geoServiceGrpChannelShutDown() {
+        if (::geoServiceGrpChannel.isInitialized) {
+            geoServiceGrpChannel.shutdown()
+            println("gRPC channel shutdown")
+        }
+    }
+
+    @Bean
+    open fun getLocationPort(
+        grpcChannel: ManagedChannel,
+    ) = GeoServiceGrpcClient(grpcChannel)
 }
--- infrastructure/src/main/kotlin/github/com/hukuta94/delivery/infrastructure/adapter/grpc/GeoServiceGrpcClient.kt
+++ infrastructure/src/main/kotlin/github/com/hukuta94/delivery/infrastructure/adapter/grpc/GeoServiceGrpcClient.kt
@@ -3,25 +3,20 @@ package github.com.hukuta94.delivery.infrastructure.adapter.grpc
 import github.com.hukuta94.delivery.core.domain.sharedkernel.Location
 import github.com.hukuta94.delivery.core.port.GetLocationPort
 import github.com.hukuta94.delivery.infrastructure.adapter.grpc.GeoGrpc.newBlockingStub
-import io.grpc.ManagedChannelBuilder
+import io.grpc.ManagedChannel
 
-class GeoServiceGrpcClient: GeoGrpc.GeoImplBase(), GetLocationPort {
+class GeoServiceGrpcClient(
+    private val managedChannel: ManagedChannel,
+): GeoGrpc.GeoImplBase(), GetLocationPort {
 
     override fun getFromStreet(street: String): Location {
-        //TODO конфигурацию адреса и порта можно вынести в конфиг
-        val channel = ManagedChannelBuilder.forAddress("localhost", 8084)
-            .usePlaintext() // disable SSL/TLS
-            .build()
-
-        val stub = newBlockingStub(channel)
+        val stub = newBlockingStub(managedChannel)
 
         val request = GetGeolocationRequest.newBuilder()
             .setStreet(street)
             .build()
 
         val response = stub.getGeolocation(request)
-
-        channel.shutdown()
 
         return Location(
             abscissa = response.location.x,
