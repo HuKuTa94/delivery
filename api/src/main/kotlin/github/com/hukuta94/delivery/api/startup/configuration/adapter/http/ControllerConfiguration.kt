09da57cf73059d379ed4d5618e9dc382168a7a84
Nikita Koshelev <hukuta94@users.noreply.github.com>
2024-07-23 00:36:51 +0300

    add CORS configuration allowing to execute GET request from backoffice application

--- api/src/main/kotlin/github/com/hukuta94/delivery/api/startup/configuration/adapter/http/ControllerConfiguration.kt
+++ api/src/main/kotlin/github/com/hukuta94/delivery/api/startup/configuration/adapter/http/ControllerConfiguration.kt
@@ -1,7 +1,11 @@
 package github.com.hukuta94.delivery.api.startup.configuration.adapter.http
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @EnableWebMvc
@@ -10,4 +14,19 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
     OrderControllerConfiguration::class,
     CourierControllerConfiguration::class,
 )
-open class ControllerConfiguration
+open class ControllerConfiguration {
+    @Bean
+    open fun corsFilter(): CorsFilter {
+        val config = CorsConfiguration().apply {
+            allowCredentials = true
+            addAllowedOrigin("http://localhost:8086")
+            addAllowedHeader("*")
+            addAllowedMethod("GET")
+        }
+        val source = UrlBasedCorsConfigurationSource().apply {
+            registerCorsConfiguration("/**", config)
+        }
+
+        return CorsFilter(source)
+    }
+}
