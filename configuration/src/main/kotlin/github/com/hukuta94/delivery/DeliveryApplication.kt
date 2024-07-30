cdfe489578b07416bf23ea2e17433a19bf34678b
Nikita Koshelev <hukuta94@users.noreply.github.com>
2024-07-30 23:45:43 +0300

    cleanup - add configuration submodule

---
+++ configuration/src/main/kotlin/github/com/hukuta94/delivery/DeliveryApplication.kt
@@ -0,0 +1,7 @@
+package github.com.hukuta94.delivery
+
+import org.springframework.boot.runApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeliveryApplicationConfiguration>(*args)
+}
