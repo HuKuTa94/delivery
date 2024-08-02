1dc7b1f895daecc4c00fed03b2ac65d3aa4b6be9
Nikita Koshelev <hukuta94@users.noreply.github.com>
2024-08-02 21:01:40 +0300

    cleanup - extract infrastructure adapter packages into separated submodules

---
+++ infrastructure/adapter/inmemory/src/main/kotlin/github/com/hukuta94/delivery/infrastructure/adapter/inmemory/CourierInMemoryRepository.kt
@@ -0,0 +1,45 @@
+package github.com.hukuta94.delivery.infrastructure.adapter.inmemory
+
+import github.com.hukuta94.delivery.core.application.port.repository.domain.CourierRepositoryPort
+import github.com.hukuta94.delivery.core.domain.courier.Courier
+import github.com.hukuta94.delivery.core.domain.courier.CourierStatus
+import java.util.*
+
+class CourierInMemoryRepository : CourierRepositoryPort {
+
+    private val storage = mutableMapOf<UUID, Courier>()
+
+    override fun add(aggregate: Courier) {
+        storage[aggregate.id] = aggregate
+    }
+
+    override fun update(aggregate: Courier) {
+        storage[aggregate.id] = aggregate
+    }
+
+    override fun update(aggregates: Collection<Courier>) {
+        aggregates.forEach { aggregate ->
+            update(aggregate)
+        }
+    }
+
+    override fun getById(id: UUID): Courier {
+        return storage[id] ?: error("The courier with id=$id is not found")
+    }
+
+    override fun existsById(id: UUID): Boolean {
+        return storage.containsKey(id)
+    }
+
+    override fun getAllFree(): Collection<Courier> {
+        return storage.values.filter { courier ->
+            courier.status == CourierStatus.FREE
+        }
+    }
+
+    override fun getAllBusy(): Collection<Courier> {
+        return storage.values.filter { courier ->
+            courier.status == CourierStatus.BUSY
+        }
+    }
+}
---
+++ infrastructure/adapter/orm/src/test/kotlin/github/com/hukuta94/delivery/infrastructure/adapter/orm/model/entity/event/OutboxJpaEntityTest.kt
@@ -0,0 +1,45 @@
+package github.com.hukuta94.delivery.infrastructure.adapter.orm.model.entity.event
+
+import github.com.hukuta94.delivery.core.application.event.domain.DomainEventClassType
+import github.com.hukuta94.delivery.core.application.event.domain.DomainEventSerializer
+import github.com.hukuta94.delivery.core.domain.order.OrderAssignedDomainEvent
+import io.kotlintest.assertSoftly
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldNotBe
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.any
+import org.mockito.kotlin.whenever
+import java.time.LocalDateTime
+import java.util.*
+
+class OutboxJpaEntityTest {
+
+    // Mocks
+    private val domainEventSerializer: DomainEventSerializer = Mockito.mock()
+
+    @Test
+    fun `creates outbox entity from domain event`() {
+        // Given
+        val serializedDomainEvent = "serialized domain event"
+        val domainEvent = OrderAssignedDomainEvent(
+            orderId = UUID.randomUUID(),
+            courierId = UUID.randomUUID(),
+        )
+        val eventType = DomainEventClassType(domainEvent::class)
+        whenever(domainEventSerializer.serialize(any()))
+            .thenReturn(serializedDomainEvent)
+
+        // When
+        val outboxEntity = OutboxEventJpaEntity.fromEvent(domainEvent, domainEventSerializer, LocalDateTime.now())
+
+        // Then
+        assertSoftly {
+            outboxEntity.id shouldBe domainEvent.id
+            outboxEntity.eventType.value shouldBe eventType.value
+            outboxEntity.payload shouldBe serializedDomainEvent
+            outboxEntity.createdAt shouldNotBe null
+            outboxEntity.processedAt shouldBe null
+        }
+    }
+}
