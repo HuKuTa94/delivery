cc57b5c8a439d8ef04a501233e16f703ae2803a7
Nikita Koshelev <hukuta94@users.noreply.github.com>
2024-08-04 21:50:12 +0300

    cleanup - add UseCase marker interface

--- core/application/main/src/main/kotlin/github/com/hukuta94/delivery/core/application/usecase/courier/MoveCouriersUseCase.kt
+++ core/application/main/src/main/kotlin/github/com/hukuta94/delivery/core/application/usecase/courier/MoveCouriersUseCase.kt
@@ -1,5 +1,7 @@
 package github.com.hukuta94.delivery.core.application.usecase.courier
 
-interface MoveCouriersUseCase {
+import github.com.hukuta94.delivery.core.application.usecase.UseCase
+
+interface MoveCouriersUseCase : UseCase {
     fun execute()
 }
