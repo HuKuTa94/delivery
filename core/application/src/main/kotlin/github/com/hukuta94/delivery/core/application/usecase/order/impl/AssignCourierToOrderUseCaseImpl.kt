b52dcebda8af1ec130c93fac491a7e3815e695dc
Nikita Koshelev <hukuta94@users.noreply.github.com>
2025-11-02 17:21:29 +0200

    fix outbox pattern implementation

--- core/application/src/main/kotlin/github/com/hukuta94/delivery/core/application/usecase/order/impl/AssignCourierToOrderUseCaseImpl.kt
+++ core/application/src/main/kotlin/github/com/hukuta94/delivery/core/application/usecase/order/impl/AssignCourierToOrderUseCaseImpl.kt
@@ -5,6 +5,7 @@ import github.com.hukuta94.delivery.core.domain.rule.DispatchOrderToCourierBusin
 import github.com.hukuta94.delivery.core.application.port.repository.domain.CourierRepositoryPort
 import github.com.hukuta94.delivery.core.application.port.repository.domain.OrderRepositoryPort
 import github.com.hukuta94.delivery.core.application.port.repository.UnitOfWorkPort
+import github.com.hukuta94.delivery.core.application.port.repository.event.OutboxEventRepositoryPort
 import org.slf4j.LoggerFactory
 
 class AssignCourierToOrderUseCaseImpl(
@@ -12,6 +13,7 @@ class AssignCourierToOrderUseCaseImpl(
     private val courierRepository: CourierRepositoryPort,
     private val dispatchOrderToCourierBusinessRule: DispatchOrderToCourierBusinessRule,
     private val unitOfWork: UnitOfWorkPort,
+    private val outboxEventRepositoryPort: OutboxEventRepositoryPort,
 ) : AssignCourierToOrderUseCase {
     override fun execute() {
         unitOfWork.executeInTransaction {
@@ -22,11 +24,12 @@ class AssignCourierToOrderUseCaseImpl(
                 return@executeInTransaction
             }
 
-
             orders.forEach { order ->
                 dispatchOrderToCourierBusinessRule.execute(order, couriers)
             }
+            val domainEvents = orders.flatMap { it.popDomainEvents() }
 
+            outboxEventRepositoryPort.saveDomainEvents(domainEvents)
             orderRepository.update(orders)
             courierRepository.update(couriers)
 
